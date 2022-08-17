0d535fdc50190b7dec285ee22b8d0c3677881863
Misha <m.belotserkovets@smc.local>
2022-08-17 19:39:11 +0300

    style fix

--- homework.py
+++ homework.py
@@ -116,8 +116,8 @@ class Swimming(Training):
         return sp / self.M_IN_KM / self.duration
 
     def get_spent_calories(self):
-        return (self.get_mean_speed() + self.CL_SWI) * self.CL_SWI_2\
-               * self.weight
+        return (self.get_mean_speed() + self.CL_SWI) * (
+                self.CL_SWI_2 * self.weight)
 
 
 def read_package(workout_type: str, data: list) -> Training:
